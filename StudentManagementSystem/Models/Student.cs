95ecb291d51204b7cc92a91865d80afe036882aa
patilrajendra9494 <patilrajendra9494@gmail.com>
2022-10-21 09:17:12 +0530

    emplyee class addedcode addess

---
+++ StudentManagementSystem/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentManagementSystem.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public DateTime date { get; set; }
+        public string Address { get; set; }
+    }
+}
