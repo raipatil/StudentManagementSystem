ec56178a34d6c5c24e2d8fbb490f89291b20bab2
patilrajendra9494 <patilrajendra9494@gmail.com>
2022-10-21 10:16:46 +0530

    Test model class added

---
+++ StudentManagementSystem/Models/Test.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentManagementSystem.Models
+{
+    public class Test
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        
+    }
+}
