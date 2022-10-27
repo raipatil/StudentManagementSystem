899437e7b010532817b82f5095e45dd62950c7f1
patilrajendra9494 <patilrajendra9494@gmail.com>
2022-10-27 10:31:27 +0530

    Add one column Phone

--- StudentManagementSystem/Models/Test.cs
+++ StudentManagementSystem/Models/Test.cs
@@ -5,6 +5,7 @@
         public virtual int Id { get; set; }
         public virtual string Name { get; set; }
         public string Address { get; set; }
+        public string Phone { get; set; }
         
     }
 }
